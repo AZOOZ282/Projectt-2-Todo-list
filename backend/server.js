3324df45cb75c67da5faf4bc30cb3fd988eb59c9
AZOOZ282 <abdulaziz.m.s.alharbi@gmail.com>
2022-07-24 14:42:27 +0300

    finish get and post

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,34 @@ const app=expres();
 const db=require("./db");
 const Todo=require("./todo");
 console.log(Todo);
+//app.use(expres.json);
+app.use(expres.json());
 
  app.get('/',(req,res)=>{
     res.json('GET / is Working')
  })
- app.get('/tasks',(req,res)=>{
-    res.json('GET / is Working')
+
+ app.get('/tasks',(req,res)=>{ 
+    Todo.find({},(err,data)=>{
+        if(err){
+            console.log('ERROR: ',err)
+        }else{
+            res.json(data)
+        }
+    });
+  });
+
+ app.post('/tasks',(req,res)=>{
+        console.log("24:",req.body)
+
+     Todo.create(req.body,(err,newTask)=>{
+            if(err){
+                console.log('ERROR: ',err)
+            }else{
+                res.status(201).json(newTask)
+            }
+        }) 
+        
  })
 
 
