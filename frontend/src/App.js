d557bbc9b186e76200932b28057f783b36b3a126
AZOOZ282 <abdulaziz.m.s.alharbi@gmail.com>
2022-07-30 11:59:23 +0300

    react starter code

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+
+const getData=()=>{
+  
+}
+
+  return (
+    <div className="App">
+      <p>app</p>
+      <button onClick={getData}>GET TASKS</button>
+    </div>
+  );
+}
